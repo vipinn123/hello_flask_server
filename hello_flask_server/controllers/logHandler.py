ecf721ba5c6228b229cc6ad7867d5b8d5dd61883
vipinn123 <vipinn@gmail.com>
2020-05-12 11:31:04 +0530

    Removed file handler for log

--- hello_flask_server/controllers/logHandler.py
+++ hello_flask_server/controllers/logHandler.py
@@ -7,8 +7,8 @@ def get_logger(logr_name):
     logger.setLevel(logging.DEBUG)
 
     # create file handler which logs the messages into a file
-    fh = logging.FileHandler('logFile.log')
-    fh.setLevel(logging.DEBUG)
+    # fh = logging.FileHandler('logFile.log')
+    # fh.setLevel(logging.DEBUG)
 
     # create console handler which logs the messages to console
     ch = logging.StreamHandler()
@@ -16,11 +16,11 @@ def get_logger(logr_name):
 
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
+    # fh.setFormatter(formatter)
     ch.setFormatter(formatter)
 
     # add the handlers to the logger
-    logger.addHandler(fh)
+    # logger.addHandler(fh)
     logger.addHandler(ch)
 
     return logger
